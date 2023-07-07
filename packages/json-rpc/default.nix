3584cb0378ec505b78e7079c6531c5fe6b7d9460
Zahary Karadjov <zahary@gmail.com>
2023-07-07 17:40:03 +0300

    Use a fixed revision of the nim-json-rpc repo

--- packages/json-rpc/default.nix
+++ packages/json-rpc/default.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "yyoncho";
   repo = "nim-json-rpc";
-  rev = "notif-changes";
+  rev = "38885260934d36ce6955a0599af1578cadaf65ec";
   hash = "sha256-eyPro3uuIZHij59dERTODt5uOtuds0V0QeQAC4K8qMs=";
 }
